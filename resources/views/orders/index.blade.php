39ded1ac139d009bdff8b22e50ba38d63e55794e
UmarMukhtaar <el.mochtaar@gmail.com>
2025-06-09 21:02:17 +0700

    minor_change

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -159,6 +159,12 @@
                                                 <a href="{{ route('orders.submit.show', $item) }}" class="inline-flex items-center px-3 py-1 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
                                                     Kirim Hasil
                                                 </a>
+                                                <button 
+                                                    @click="selectedItem = {{ json_encode($item->toArray()) }}; showRejectModal = true" 
+                                                    class="inline-flex items-center px-3 py-1 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
+                                                >
+                                                    Batalkan
+                                                </button>
                                             @endif
                                         </div>
                                     </div>
